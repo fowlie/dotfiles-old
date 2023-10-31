cd11b9d3d044d7491f6273e311041b0dc0332d9e
Mats Faugli <mats.faugli@dnb.no>
2023-10-31 09:50:01 +0100

    feat(fish): add aws-switch-region alias with `ar` abbreviation

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,7 @@ set -x PATH $HOME/go/bin $PATH
 set -x PATH /opt/homebrew/bin $PATH
 set -x EDITOR /opt/homebrew/bin/nvim
 set -x AWS_PROFILE dev
+set -x AWS_REGION eu-north-1
 
 # used for syntax highlighting in lf previews
 # see https://xyproto.github.io/splash/docs/all.html
@@ -14,33 +15,36 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 
     # Abbreviations
-    abbr --add --global -- aws-tail 'aws logs describe-log-groups | jq '\''.logGroups[].logGroupName'\'' -r | fzf | xargs aws logs tail --format short --since 1h'
-    abbr --add --global -- aws-tailf 'aws logs describe-log-groups | jq '\''.logGroups[].logGroupName'\'' -r | fzf | xargs aws logs tail --format short --follow --since 1h'
     abbr --add --global -- ciam-docker-login 'aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 364737385840.dkr.ecr.eu-west-1.amazonaws.com'
     abbr --add --global -- ciam-psql 'docker exec -it db-migrate_postgres_1 psql -U postgres ciam'
     abbr --add --global -- cc ciam-curl
     abbr --add --global -- copy 'xclip -o -selection clipboard'
     abbr --add --global -- d docker
     abbr --add --global -- dc docker-compose
+
+    # Git abbreviations
     abbr --add --global -- g git
     abbr --add --global -- gap 'git add -p'
     abbr --add --global -- gcm 'git commit -m'
     abbr --add --global -- gd 'git diff'
     abbr --add --global -- gs 'git status'
     abbr --add --global -- gsw 'git switch'
-    abbr --add --global -- hms 'home-manager switch'
     abbr --add --global -- lg lazygit
-    abbr --add --global -- zj zellij
-    abbr --add --global -- zjc zellij 'zellij --layout compact'
-    abbr --add --global -- asp aws-switch-profile
-    abbr --add --global -- asl 'aws sso login'
+
+    # AWS abbreviations
+    abbr --add --global -- aws-tail 'aws logs describe-log-groups | jq '\''.logGroups[].logGroupName'\'' -r | fzf | xargs aws logs tail --format short --since 1h'
+    abbr --add --global -- aws-tailf 'aws logs describe-log-groups | jq '\''.logGroups[].logGroupName'\'' -r | fzf | xargs aws logs tail --format short --follow --since 1h'
+    abbr --add --global -- ap aws-switch-profile
+    abbr --add --global -- ar aws-switch-region
+    abbr --add --global -- al 'aws sso login'
 
     # Aliases
     alias ... 'cd ../..'
     alias .... 'cd ../../..'
     alias aws-switch-profile 'set -x AWS_PROFILE (cat ~/.aws/config | grep "\[" | grep -v default | tr -d "[]" | awk "{print \$2}" | fzf --header "AWS Profiles" --height 6 --reverse --bind one:accept)'
+    alias aws-switch-region 'set -x AWS_REGION (echo -e "eu-north-1\neu-west-1" | fzf --header "AWS Regions" --height 6 --reverse --bind one:accept)'
     alias ls 'exa --icons --oneline --group-directories-first'
-    alias ll 'exa --icons --long --group-directories-first --time-style long-iso'
+    alias ll 'exa --all --icons --long --group-directories-first --time-style long-iso'
 
     # https://news.ycombinator.com/item?id=11071754
     alias config '/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
